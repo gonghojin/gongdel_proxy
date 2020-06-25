27fe01218dfc0863347db2982aed93c4d81ceb6f
gonghojin <yolip51@naver.com>
2020-06-25 21:45:37 +0900

    교육 완료

--- webpack.config.js
+++ webpack.config.js
@@ -48,8 +48,9 @@ module.exports = {
             favicon: 'public/favicon.ico'
         })
     ],
-    // TODO: 여기에 api 서버(localhost:8001) 프록싱 설정을 추가하세요
-    /**
-     * 설정 후,  yarn dev 명령어를 사용하세요
-     */
+    devServer: {
+        proxy: {
+            '/api': 'http://localhost:8001', // 프록시
+        }
+    }
 };
